c61c30bd904546d1cc4663a7e8c057021f46a181
Ratnam Solutions <manikanta@ratnamsolutions.com>
2024-12-11 10:18:31 +0530

    Initial commit

---
+++ test/ecrproj.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ecrproj from '../lib/ecrproj-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecrproj-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ecrproj.EcrprojStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
